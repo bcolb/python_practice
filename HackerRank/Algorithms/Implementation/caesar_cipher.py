2d8e87cff746213963875fd66bcdbb6417b425d8
Brice <brice.tech@gmail.com>
2015-10-29 20:29:36 -0700

    Modified solution for caesar_cipher

--- HackerRank/Algorithms/Implementation/caesar_cipher.py
+++ HackerRank/Algorithms/Implementation/caesar_cipher.py
@@ -1,30 +1,23 @@
 def shift_letter(letter, k):
-    curr_ord = ord(letter)
-    k = k % 26
-    if curr_ord >= 97 and curr_ord <= 122:
-        new_ord = curr_ord + k
-        if new_ord > 122:
-            diff = new_ord % 123
-            new_ord = 97 + diff
-        return chr(new_ord)
-    elif curr_ord >= 65 and curr_ord <= 90:
-        new_ord = curr_ord + k
-        if new_ord > 90:
-            diff = new_ord % 91
-            new_ord = 65 + diff
-            return chr(new_ord)
+    if letter.isalpha():
+        lowend = 97
+        highend = 122
+        if letter.isupper():
+            lowend = 65
+            highend = 90
+        k = k % 26
+        new_ord = ord(letter) + k
+        if new_ord > highend:
+            new_ord = lowend + (new_ord % (highend + 1))
         return chr(new_ord)
     else:
         return letter
-
+    
 text_length = int(input())
 text = input()
 k = int(input())
-
 new_text = ""
-
 for letter in text:
     new_text += shift_letter(letter, k)
-
 print(new_text)
                 
